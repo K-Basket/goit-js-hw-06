5cb282f5d49a8f980e9e6fde23a9f3c64f6efb50
K-Basket <koshykkv@gmail.com>
2023-02-13 14:31:25 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const elements = ingredients.map(el => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = el;
+  itemRef.classList.add('item');
+
+  return itemRef;
+});
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+ingredientsRef.append(...elements);
