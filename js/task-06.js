056e4dccc7e682703d0fbcf01a3be63b78f360c5
K-Basket <koshykkv@gmail.com>
2023-02-15 18:21:01 +0200

    task-07

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const inputRef = document.querySelector('#validation-input');
+
+inputRef.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+  if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
+    inputRef.style.borderColor = '#4caf50';
+  } else {
+    inputRef.style.borderColor = '#f44336';
+  }
+}
