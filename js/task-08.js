d205c3b7f87ac6fdd4fef608e382385d88d2b4ba
K-Basket <koshykkv@gmail.com>
2023-02-15 20:10:38 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onControlForm);
+
+function onControlForm(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Внимание! Все поля должны быть заполнены');
+  }
+
+  const elements = {
+    Email: `${email.value}`,
+    Password: `${password.value}`,
+  };
+  console.log(elements);
+
+  event.currentTarget.reset();
+}
