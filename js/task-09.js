06fd11d459c4116ae23231e59632ca86de0561d3
K-Basket <koshykkv@gmail.com>
2023-02-15 22:29:00 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyRef = document.querySelector('body');
+const changeColorRef = document.querySelector('.change-color');
+const valueColorRef = document.querySelector('.color');
+
+changeColorRef.addEventListener('click', onChangeBackgroundColor);
+
+function onChangeBackgroundColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  valueColorRef.textContent = getRandomHexColor();
 }
